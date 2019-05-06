664fb9d49eace2f43b7b3b9f5bf1ea4f32380c8b
Bolke de Bruin <bolke@xs4all.nl>
2019-05-06 20:08:43 +0200

    Make versioning more flexible

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import find_packages
 setup(
     name='autokeras_tabular',
     packages=find_packages(exclude=('tests',)),
-    install_requires=['autokeras==0.3.7',
-                      'lightgbm==2.2.3',
-                      'numpy==1.16.1',],
-    version='0.3.7',
+    install_requires=['autokeras>=0.3.7',
+                      'lightgbm>=2.2.3',
+                      'numpy>=1.16.1'],
+    version='0.3.7.1',
     description='AutoML Tabular addon for deep learning',
     author='Original Author: DATA Lab at Texas A&M University',
     author_email='bdbruin@gmail.com',
-    url='http://github.com/bolkedebruin/autokeras-tabular',
-    download_url='https://github.com/bolkedebruin/autokeras_tabular/archive/0.3.7.tar.gz',
+    url='http://github.com/bolkedebruin/autokeras_tabular',
+    download_url='https://github.com/bolkedebruin/autokeras_tabular/archive/0.3.7.1.tar.gz',
     keywords=['AutoML', 'keras', 'tabular'],
     classifiers=[]
 )
